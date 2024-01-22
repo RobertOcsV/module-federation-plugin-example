109ceaba639f35f3bc1f78911eb68e026aa24b3f
RobertOcsV <robertoliveiracor@hotmail.com>
2024-01-21 23:42:47 -0300

    feat: explicit export dependences between fronts

--- projects/mfe1/webpack.config.js
+++ projects/mfe1/webpack.config.js
@@ -8,8 +8,29 @@ module.exports = withModuleFederationPlugin({
     './Module': './projects/mfe1/src/app/flights/flights.module.ts',
   },
 
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
+  shared: share({
+    '@angular/core': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common/http': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/router': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+  }),
 
+  // Explicitly share mono-repo libs:
+  sharedMappings: ['auth-lib'],
 });
--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -9,5 +9,30 @@ module.exports = withModuleFederationPlugin({
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
+   shared: share({
+    '@angular/core': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common/http': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/router': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+  }),
+
+  // Explicitly share mono-repo libs:
+  sharedMappings: ['auth-lib'],
 
 });
